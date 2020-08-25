5ca7df36558727dfc8aad03a922554240389185e
Micky <mickeylock@Mickys-MacBook-Pro.local>
2020-08-25 22:59:45 +0300

    created cat flatlist

--- App.js
+++ App.js
@@ -1,21 +1,94 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+  Image,
+  SafeAreaView,
+} from "react-native";
 
-export default function App() {
+const mediaArray = [
+  {
+    key: "0",
+    title: "Title 1",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales enim eget leo condimentum vulputate. Sed lacinia consectetur fermentum. Vestibulum lobortis purus id nisi mattis posuere. Praesent sagittis justo quis nibh ullamcorper, eget elementum lorem consectetur. Pellentesque eu consequat justo, eu sodales eros.",
+    thumbnails: {
+      w160: "http://placekitten.com/160/161",
+    },
+    filename: "http://placekitten.com/2048/1920",
+  },
+  {
+    key: "1",
+    title: "Title 2",
+    description:
+      "Donec dignissim tincidunt nisl, non scelerisque massa pharetra ut. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Vestibulum tincidunt sapien eu ipsum tincidunt pulvinar. ",
+    thumbnails: {
+      w160: "http://placekitten.com/160/164",
+    },
+    filename: "http://placekitten.com/2041/1922",
+  },
+  {
+    key: "2",
+    title: "Title 3",
+    description:
+      "Phasellus imperdiet nunc tincidunt molestie vestibulum. Donec dictum suscipit nibh. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. ",
+    thumbnails: {
+      w160: "http://placekitten.com/160/167",
+    },
+    filename: "http://placekitten.com/2039/1920",
+  },
+];
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.screen}>
+      {/*       <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" /> */}
+      <FlatList
+        data={mediaArray}
+        renderItem={({ item }) => {
+          return (
+            <View style={styles.gridItem}>
+              <TouchableOpacity>
+                <Image
+                  style={{ flex: 1, width: 100, height: 100 }}
+                  source={{ uri: item.thumbnails.w160 }}
+                />
+                <View style={{ flex: 2, marginHorizontal: 20 }}>
+                  <Text style={styles.titleText}>{item.title}</Text>
+                  <Text>{item.description}</Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+          );
+        }}
+      />
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+  },
+  gridItem: {
+    backgroundColor: "#D3D3D3",
+    marginVertical: 2,
+  },
+  titleText: {
+    fontWeight: "bold",
+  },
+
+  textContainer: {
+    flexDirection: "row",
   },
 });
+
+export default App;
