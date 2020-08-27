a486d801043c09072c7f27b1862a1170c376a0a8
Micky <mickeylock@Mickys-MacBook-Pro.local>
2020-08-27 11:04:48 +0300

    added top cat imagge

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import {createStore, combineReducers, applyMiddleware} from 'redux';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import {Provider} from 'react-redux';
 import React from 'react';
-import {StyleSheet, SafeAreaView} from 'react-native';
+import {Image, Dimensions, StyleSheet, View, SafeAreaView} from 'react-native';
 import List from './components/List';
 
 import placesReducer from './store/places-reducer';
@@ -14,6 +14,14 @@ const App = () => {
   return (
     <Provider store={store}>
       <SafeAreaView style={styles.screen}>
+        <View style={styles.imageContainer}>
+
+          <Image
+            source={require('./assets/homeless_cat.jpg')}
+            style={styles.image}
+          />
+
+        </View>
         <List />
         <StatusBar style='auto' />
       </SafeAreaView>
@@ -22,6 +30,20 @@ const App = () => {
 };
 
 const styles = StyleSheet.create({
+  imageContainer: {
+    width: Dimensions.get('window').width * 1,
+    height: Dimensions.get('window').width * 0.5,
+    borderRadius: (Dimensions.get('window').width * 0.3) / 2,
+    borderWidth: 3,
+    borderColor: 'black',
+    overflow: 'hidden',
+    marginVertical: Dimensions.get('window').height / 20,
+    marginHorizontal: Dimensions.get('window').width / 20,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
   screen: {
     flex: 1,
     alignItems: 'center',
